e30dc0e9bf53f2d33d1a45ddd9a8c3e26f6b3929
Mark Gray <mfgray87@gmail.com>
2018-07-21 13:56:26 +0100

    added predictions, tricks and updating scoreboard

--- code.py
+++ code.py
@@ -14,7 +14,6 @@ class Card():
 		return '%s%s' %(Card.ranks[self.rank],Card.suits[self.suit_index])
 
 
-
 class Deck():
 	def __init__(self):
 		self.cards = []
@@ -35,6 +34,7 @@ class Deck():
 		self.card = self.cards.pop()
 		return self.card
 
+
 class Player():
 	member_id_count = 0
 	def __init__(self):
@@ -46,11 +46,11 @@ class Player():
 
 	def register(self, game):
 		self.game = game		
-		game.scoreboard[self.username] = 0
+		game.scoreboard[self] = 0
 		current_player_count = len(game.entrants)
 		game.seats[current_player_count] = self
 		game.entrants.append(self)
-	
+		game.trick_count[self] = 0	
 
 	def playCard(self, card):		
 		if Dealer.is_valid_move(card, self.game.pile, self.hand) ==  "first card":
@@ -60,10 +60,12 @@ class Player():
 		elif Dealer.is_valid_move(card, self.game.pile, self.hand) ==  True and card in self.hand:						
 			self.game.pile[self] = card
 			self.hand.remove(card)
-		else: raise ValueError('INVALID MOVE')
+		else: 
+			raise ValueError('invalid move')			
 
 	def __repr__(self):
 		return self.username
+
 
 class Dealer():
 	@classmethod
@@ -72,20 +74,34 @@ class Dealer():
 		if len(pile) == 0:
 			return "first card"
 		else:
-			for card in hand:
-				if card.suit == pile ['valid_suit']:
-					available_cards.append(card)
+			for i in hand:
+				if i.suit == pile ['valid_suit']:
+					available_cards.append(i)
 		if card.suit == pile['valid_suit']:
 			return True
 		elif len(available_cards) == 0:
 			return True		
-		else: 
+		else:
 			return False
 
 	@classmethod
 	def declareWinner(cls, game):
-		game.pile.clear()
-		
+		cards = []
+		card_ranks = []
+		valid_suit = game.pile['valid_suit']
+		del game.pile['valid_suit'] 	
+		for i in game.pile.values():
+			if i.__class__ == Card and i.suit == valid_suit:
+				cards.append(i)
+		for i in cards:
+			card_ranks.append(i.rank)
+		for player in game.pile.keys():
+			if game.pile[player].rank == max(card_ranks) and game.pile[player] in cards:
+				print ""
+				print "%s wins this trick" %(player)
+				print ""
+				game.trick_count[player] += 1
+		game.pile.clear()		
 
 	@classmethod
 	def requestActions(cls, game):
@@ -94,15 +110,36 @@ class Dealer():
 			player = game.seats[active_player]
 			print "%s's Cards %s" %(player, player.hand)
 			chosen_card = int(raw_input("Pick Your Card Please %s :  " %player))			
-			player.playCard(player.hand[chosen_card])
+			try:
+				player.playCard(player.hand[chosen_card])
+			except:
+				print "%s's Cards %s" %(player, player.hand)
+				chosen_card = int(raw_input("Invalid. Pick a different card please %s :  " %player))
+				player.playCard(player.hand[chosen_card])
 			print ""
 			print "pile: %s" %(game.pile)
 			print ""
 			active_player += 1
 
 	@classmethod
+	def requestPredictions(cls, game):
+		active_player = 0		
+		for player in range(game.size):
+			player = game.seats[active_player]
+			chosen_card = int(raw_input("%s How many tricks do you think you'll win:  " %player))
+			game.predictions[player] = chosen_card
+			game.trick_count[player] = 0
+			active_player += 1
+		print ""
+		print "--Predictions--"
+		print game.predictions
+		print ""
+
+	@classmethod
 	def printGameState(cls, game):
-		print "-----------------------GAMEROUND %s-----------------------" %(game.round-1)
+		print "----------------------------------------------GAMEROUND %s----------------------------------------------" %(game.round-1)
+		print ""
+		print "---------------------subround %s---------------------"%(game.subround)
 		print ""
 		print "---PLAYERS---"
 		print ""
@@ -120,21 +157,33 @@ class Dealer():
 	@classmethod
 	def dealCards(cls, game):
 		for i in range(game.round):
+			game.subround += 1
 			for player in game.entrants:
 				card = game.deck.randomCard()
 				player.hand.append(card)
 		game.round += 1
 
 	@classmethod
+	def updateLeaderboard(cls, game):
+		for player in game.entrants:
+			if game.predictions[player] == game.trick_count[player]:
+				game.scoreboard[player] += game.predictions[player] * 10
+			else: game.scoreboard[player] -= game.predictions[player] * 10
+
+	@classmethod
 	def start_game(cls, game):
 		for i in range(game.length):
 		#single method that runs the whole game			
 			Dealer.dealCards(game)
+			game.subround = 1
 			Dealer.printGameState(game)
+			Dealer.requestPredictions(game)
 			for i in range(game.round-1):
 				Dealer.requestActions(game)
+				game.subround += 1
 				Dealer.printGameState(game)
 				Dealer.declareWinner(game)
+			Dealer.updateLeaderboard(game)
 
 class Game():
 	deck = Deck()
@@ -148,6 +197,9 @@ class Game():
 		self.pile = collections.OrderedDict()
 		self.seats = {}
 		self.round = 1
+		self.subround = 1
+		self.predictions = {}
+		self.trick_count = {}
 
 	def __repr__(self):
 		#human readable representation of game object		
@@ -195,6 +247,7 @@ mike.register(test_game)
 
 
 Dealer.start_game(test_game)
+print test_game.__dict__
 
 
 
